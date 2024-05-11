3a851dbec8998d7824f361cff321f54294e87087
Suwashshrestha <86812244+Suwashshrestha@users.noreply.github.com>
2024-05-11 12:49:47 +0545

    Update index.js

---
+++ passenger_counter/index.js
@@ -0,0 +1,28 @@
+// let count = 5
+
+// count = count +1
+
+
+let count =0
+// console.log(count)
+function increament(){
+    console.log('clicked')
+    count = count + 1
+    document.getElementById("count").innerHTML= count;
+}
+ 
+function decreament(){
+    console.log('clicked')
+    count = count - 1
+    document.getElementById("count").innerHTML= count;
+}
+function save(){
+        let counter = count + " - "
+        document.getElementById("save-el").textContent += counter
+        document.getElementById("count").textContent = 0
+        count = 0
+
+    
+    
+
+}
